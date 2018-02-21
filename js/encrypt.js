c53fc6d382d81ecf3539554718f991122c86c875
srednja-skola <dgavrano@gmail.com>
2018-02-21 17:31:30 +0100

    Add files via upload

---
+++ js/encrypt.js
@@ -0,0 +1,14 @@
+// encrypt
+var pokrivac = document.querySelector(".pokrivac");
+
+function sifra() {
+    var unos = prompt("Upiši šifru kako bi vidio sadržaj");
+    if (unos === null || unos === undefined || unos === "") {
+       pokrivac.classList = "pokrivac";
+    } else if (unos === "alfa") {
+        pokrivac.classList = "";
+    } else {
+        pokrivac.classList = "pokrivac";
+    }
+}
+sifra();
--- js/script.js
+++ js/script.js
@@ -53,18 +53,3 @@ function rippleEffect() {
     }
 }
 rippleEffect();
-
-// encrypt
-var pokrivac = document.querySelector(".pokrivac");
-
-function sifra() {
-    var unos = prompt("Upiši šifru kako bi vidio sadržaj");
-    if (unos === null || unos === undefined || unos === "") {
-       pokrivac.classList = "pokrivac";
-    } else if (unos === "alfa") {
-        pokrivac.classList = "";
-    } else {
-        pokrivac.classList = "pokrivac";
-    }
-}
-sifra();
