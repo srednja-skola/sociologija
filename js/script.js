4c545fa2ab0044126db2acb8be1577f4122755e1
srednja-skola <dgavrano@gmail.com>
2018-06-16 18:21:21 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -78,7 +78,13 @@ route.add("", function() {
   // footer
   copyleft.innerHTML = "Copyleft <span class='cl'>&copy;</span> " + new Date().getFullYear() + ". - Danijel Gavranović, prof.";
   // izbornik
-  izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija - šk.god. 2017./18.</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='ispitivanje.html'><i class='fa fa-exclamation-circle ikonica'></i> <span class='tekst-izbornik'>Ispitivanje</span></a></li><li><a href='istrazivanje.html'><i class='fa fa-bar-chart-o ikonica'></i> <span class='tekst-izbornik'>Istraživanje</span></a></li><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li><li><a href='stare-zadace.html' title='Stare zadaće'><i class='fa fa-file-word-o ikonica'></i> <span class='tekst-izbornik'>Stare zadaće</span></a> </li><li><a href='pitaj-profesora.html' title='Pitaj profesora'><i class='fa fa-question-circle ikonica' ></i> <span class='tekst-izbornik'>Pitaj profesora</span></a></li></ul></div></div>";
+
+/*
+
+izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija - šk.god. 2017./18.</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='ispitivanje.html'><i class='fa fa-exclamation-circle ikonica'></i> <span class='tekst-izbornik'>Ispit</span></a></li><li><a href='istrazivanje.html'><i class='fa fa-bar-chart-o ikonica'></i> <span class='tekst-izbornik'>Istraživanje</span></a></li><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li><li><a href='stare-zadace.html' title='Stare zadaće'><i class='fa fa-file-word-o ikonica'></i> <span class='tekst-izbornik'>Stare zadaće</span></a> </li><li><a href='pitaj-profesora.html' title='Pitaj profesora'><i class='fa fa-question-circle ikonica' ></i> <span class='tekst-izbornik'>Pitaj profesora</span></a></li></ul></div></div>";
+
+*/
+
 // aktivni link na izborniku
   if (window.location.href.indexOf("ispitivanje.html") > -1) {
     $(".nav > li:nth-child(1) > a:nth-child(1)").addClass("active");
@@ -95,7 +101,9 @@ route.add("", function() {
   } else {
     console.log("nema aktivnog linka");
 }
+
 });
+
 route.add("pptx.html", function() {
   // footer s linkom na mom imenu
   function sifra() {
