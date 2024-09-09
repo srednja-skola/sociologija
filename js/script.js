38203e4f860c9fd7fa8829ff5079eaff8331e342
srednja-skola <dgavrano@gmail.com>
2024-09-09 18:58:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,13 +80,13 @@ route.add("", function() {
   // izbornik
 
  // izbornik master (bez linka na istraživanje i pitaj prof.)
-/* izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li></ul></div></div>"; */
+izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li></ul></div></div>"; 
   
 // izbornik bez linka na istraživanje s linkom pitaj profesora
 /* izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li><li><a href='pitaj-profesora.html' title='Pitaj profesora'><i class='fa fa-question-circle ikonica' ></i> <span class='tekst-izbornik'>Pitaj profesora</span></a></li></ul></div></div>"; */
     
 // izbornik s linkom istraživanje bez pitaj profesora  
-izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='istrazivanje.html'><i class='fa fa-bar-chart-o ikonica'></i> <span class='tekst-izbornik'>Istraživanje</span></a></li><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li></ul></div></div>"; 
+/* izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'><span class='sr-only'>Pokaži izbornik</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='index.html'><i class='fa fa-home ikonica'></i> Sociologija</a></div> <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav desno'><li><a href='istrazivanje.html'><i class='fa fa-bar-chart-o ikonica'></i> <span class='tekst-izbornik'>Istraživanje</span></a></li><li><a href='prezentacije.html'><i class='fa fa-image ikonica'></i> <span class='tekst-izbornik'>Prezentacije</span></a></li><li><a href='dodatni-materijal.html' title='Dodatni materijali'><i class='fa fa-hand-spock-o ikonica'></i> <span class='tekst-izbornik'>Dodatni materijal</span></a></li></ul></div></div>"; */
     
 // full izbornik (ispitivanje i istraživanje html)
 /*
@@ -94,17 +94,17 @@ izbornik.innerHTML = "<div class='container'><div class='navbar-header'><button 
 */
 
 // aktivni link u izborniku (manji izbornik na početku šk. godine bez linka istraživanje i pitaj profesora)
-/* if (window.location.href.indexOf("prezentacije.html") > -1) {
+if (window.location.href.indexOf("prezentacije.html") > -1) {
   $(".nav > li:nth-child(1) > a:nth-child(1)").addClass("active");
 } else if (window.location.href.indexOf("dodatni-materijal.html") > -1) {
   $(".nav > li:nth-child(2) > a:nth-child(1)").addClass("active");
 } else {
   console.log("nema aktivnog linka");
-} */
+}
  
     
 // aktivni link u izborniku (s linkom na istraživanje)
-if (window.location.href.indexOf("istrazivanje.html") > -1) {
+/*  if (window.location.href.indexOf("istrazivanje.html") > -1) {
   $(".nav > li:nth-child(1) > a:nth-child(1)").addClass("active");
 } else if (window.location.href.indexOf("prezentacije.html") > -1) {
   $(".nav > li:nth-child(2) > a:nth-child(1)").addClass("active");
@@ -118,7 +118,7 @@ if (window.location.href.indexOf("istrazivanje.html") > -1) {
 //   $(".nav > li:nth-child(4) > a:nth-child(1)").addClass("active");
 } else {
   console.log("nema aktivnog linka");
-}  
+}   */
 
 // aktivni link na izborniku (za full izbornik)
 //   if (window.location.href.indexOf("ispitivanje.html") > -1) {
